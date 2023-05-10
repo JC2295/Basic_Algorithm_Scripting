1a2a3eec110fff9c3c74c033c747698bdb7ba8cf
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-05-10 13:04:02 +0000

    Completed Challenge

---
+++ finders-keepers.js
@@ -0,0 +1,15 @@
+function findElement(arr, func) {
+
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
+
+  return undefined;
+
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//Challenge Completed
