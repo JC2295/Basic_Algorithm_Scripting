0a5bc3b345926feb8a58b06c0be78bb14beb5ab7
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-05-11 09:05:11 +0000

    Completed some work on mutations algorithm

---
+++ mutations.js
@@ -0,0 +1,23 @@
+function mutation(arr) {
+
+  let arr1 = arr[0].toLowerCase();
+
+  let arr2 = arr[1].toLowerCase();
+
+  console.log(arr1);
+
+  console.log(arr2);
+
+  for (let i = 0; i < arr2.length; i++) {
+
+    for (let j = 0; j < arr1.length; j++) {
+
+      //come back to this one
+
+    }
+
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
